da76bbcd6e10c6b6c6bf77139ed2a6762f49e6d1
BJ Paat <brpaat@ayannah.com>
2017-06-21 11:51:10 +0800

    Use django environ for Database settings
    Environment variables can be placed in the prod.py or local.py.

--- mayannah/config/settings/local.py
+++ mayannah/config/settings/local.py
@@ -1,9 +1,22 @@
 from .base import *
+import environ
 
-DEBUG=True
+root = environ.Path(__file__) -3
+env = environ.Env()
+environ.Env.read_env(root.path('local.env')())
+
+DEBUG = True
+
 
 INSTALLED_APPS += [
     'partner',
     'rest_framework',
 ]
 ALLOWED_HOSTS = ['*']
+
+# DATABASE CONFIG
+DATABASES = {
+        'default': env.db(),
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
