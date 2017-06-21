6a527c5d22f9b5f89184a9b4427b17a31d783321
BJ Paat <brpaat@ayannah.com>
2017-06-21 16:13:30 +0800

    Add collectstatic for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 python manage.py migrate
+python manage.py collectstatic --noinput
 echo Starting Gunicorn.
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
     --name docker_mayannah \
     --workers 2 \
-    --log-level=info\
+    --log-level=info \
 "$@"
 echo ohmmm
