2829f2d314530cd36e71f974a5d68dee6f1c3a96
BJ Paat <brpaat@ayannah.com>
2017-06-27 17:54:45 +0800

    Add Virtual Money Models

--- mayannah/virtual_money/admin.py
+++ mayannah/virtual_money/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from virtual_money.models import Transaction
 
-# Register your models here.
+admin.site.register(Transaction)
--- mayannah/virtual_money/models.py
+++ mayannah/virtual_money/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class DepositManager(models.Manager):
+    """Filter deposit transactions only."""
+    def get_queryset(self):
+        return super(DepositManager,
+                self).get_queryset().filter(is_deposit=True)
+
+class WithdrawManager(models.Manager):
+    """Filter not deposit transactions only."""
+    def get_queryset(self):
+        return super(DepositManager,
+                self).get_queryset().filter(is_deposit=False)
+
+
+class Transaction(models.Model):
+    reference_id = models.CharField(max_length=255, unique=True)
+    user = models.ForeignKey(User)
+    amount = models.IntegerField()
+    is_deposit = models.BooleanField(default=True)
+
+    # Model Managers
+    objects = models.Manager()
+    deposits = DepositManager()
+    withdraws = WithdrawManager()
+
+    def __str__(self):
+        return self.reference_id
+
