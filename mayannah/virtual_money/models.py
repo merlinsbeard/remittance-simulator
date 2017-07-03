7e65185ba4d5a12ee510f7b2d63f98ad0b1f6e5d
BJ Paat <brpaat@ayannah.com>
2017-07-03 09:00:40 +0800

    Update model to contain details
    Receiver and Sender points to User. Theyre the objects
    that exchange transactions.
    
    Status field has fixed values. This makes it easier to
    validate and check values.
    
    Date created and date updated is created. This will be
    used to filter objects based on date.

--- mayannah/virtual_money/models.py
+++ mayannah/virtual_money/models.py
@@ -2,24 +2,39 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+STATUS = (
+        ("AVAILABLE", "AVAILABLE"),
+        ("COMPLETE", "COMPLETE"),
+        ("PAID", "PAID"),
+        ("CANCELLED", "CANCELLED"),
+)
+
+
 class DepositManager(models.Manager):
     """Filter deposit transactions only."""
     def get_queryset(self):
         return super(DepositManager,
-                self).get_queryset().filter(is_deposit=True)
+                     self).get_queryset().filter(is_deposit=True)
+
 
 class WithdrawManager(models.Manager):
     """Filter not deposit transactions only."""
     def get_queryset(self):
         return super(DepositManager,
-                self).get_queryset().filter(is_deposit=False)
+                     self).get_queryset().filter(is_deposit=False)
 
 
 class Transaction(models.Model):
     reference_id = models.CharField(max_length=255, unique=True)
-    user = models.ForeignKey(User)
+    receiver = models.ForeignKey(User, related_name='receiver')
+    sender = models.ForeignKey(User, related_name='sender')
     amount = models.IntegerField()
+    status = models.CharField(max_length=100,
+                              choices=STATUS,
+                              default="AVAILABLE")
     is_deposit = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     # Model Managers
     objects = models.Manager()
@@ -28,4 +43,3 @@ class Transaction(models.Model):
 
     def __str__(self):
         return self.reference_id
-
