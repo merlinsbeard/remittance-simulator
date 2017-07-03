672bba98ef9a0e515c2170aa62b6e96ea452117a
BJ Paat <brpaat@ayannah.com>
2017-07-03 09:12:32 +0800

    Add views for transaction api v1 endpoint
    All endpoints needs to have a basic authentication.
    
    * TransactionDetail
      Is used to show a single transaction based on
      reference_id. It only accepts GET
    
    * TransactionCreate
      Used for creating a transaction.
    
    * TransactionPay
      Change the status of transaction to pay

--- mayannah/v1/views.py
+++ mayannah/v1/views.py
@@ -1,15 +1,19 @@
 from partner.models import Remittance
-from .serializers import RemittanceSerializer, RemittancePaySerializer
+from .serializers import RemittanceSerializer, RemittancePaySerializer, \
+                         TransactionDetailSerializer, TransactionSerializer, \
+                         TransactionCompleteSerializer
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from virtual_money.models import Transaction
 
 import arrow
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 class RemittanceList(generics.ListCreateAPIView):
     """View and Create Remittance Transaction.
@@ -113,3 +117,83 @@ class RemittancePay(generics.GenericAPIView):
             return Response({"message": message})
         else:
             return Response({"FAIL": "FAIL"})
+
+
+class TransactionDetail(generics.RetrieveAPIView):
+    serializer_class = TransactionDetailSerializer
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    lookup_field = 'reference_id'
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(sender=user)
+
+class TransactionCreate(generics.CreateAPIView):
+    serializer_class = TransactionSerializer
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+def status_check(status):
+    codes = {
+            "AVAILABLE": {
+                "status": "Success",
+                "code": status.HTTP_200_OK},
+            "PAID": {
+                "status": "Already Paid Out",
+                "code": status.HTTP_200_OK},
+            "CANCELLED": {
+                "status": "Transaction Cancelled",
+                "code": status.HTTP_423_LOCKED},
+            "ERROR": {
+                "status": "ERROR",
+                "code": status.HTTP_404_NOT_FOUND},
+            }
+
+    for k, v in codes.items():
+        logger.warning(f'k{k}-v{v}')
+        if k == remittance_status:
+            return v
+    else:
+        return codes['ERROR']
+
+class TransactionPay(generics.UpdateAPIView):
+    serializer_class = TransactionCompleteSerializer
+    authentication_classes = (BasicAuthentication,)
+
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        transaction = data['reference_id']
+
+        message = {"status": "Success",
+                "message": "Blank"}
+
+        try:
+            transaction = Transaction.objects.get(reference_id=transaction)
+        except Transaction.DoesNotExist:
+            message['status'] = 'Error'
+            message['message'] = "Does Not exists"
+            return Response(message, status.HTTP_404_NOT_FOUND)
+
+        if transaction.status == "PAID":
+            return Response(
+                    {"status": "Already Paid"},
+                    status.HTTP_200_OK)
+        elif transaction.status != "AVAILABLE":
+            return Response(
+                    {"status": "Not available"},
+                    status.HTTP_404_NOT_FOUND)
+        else:
+            """Pay Remittance"""
+            transaction.status = "PAID"
+            now = arrow.now().datetime
+            transaction.save()
+            message = "Successfully Tagged as Paid"
+        return Response({"message": message})
