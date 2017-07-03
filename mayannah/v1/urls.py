cc0c487658b8e9cb06d120e5a35ffc3a934c15d8
BJ Paat <brpaat@ayannah.com>
2017-07-03 09:17:48 +0800

    Add urls for transaction api
    Available endpoints:
    POST /transaction/
    POST /transaction/pay
    GET /transaction/<reference_id>

--- mayannah/v1/urls.py
+++ mayannah/v1/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from v1.views import RemittanceList, RemittanceDetail, RemittancePay
+from v1.views import RemittanceList, RemittanceDetail, RemittancePay, \
+                     TransactionDetail, TransactionCreate, TransactionPay
 
 urlpatterns = [
         url(r'^remittance/$', RemittanceList.as_view()),
         url(r'^remittance/(?P<slug>[-\w]+)/$', RemittanceDetail.as_view()),
         url(r'^remittance/(?P<slug>[-\w]+)/pay/$',
                     RemittancePay.as_view()),
+        # Transactions
+        url(r'^transaction/$', TransactionCreate.as_view()),
+        url(r'^transaction/complete/$', TransactionPay.as_view()),
+        url(r'^transaction/(?P<reference_id>[-\w]+)/$', TransactionDetail.as_view()),
+
         ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
